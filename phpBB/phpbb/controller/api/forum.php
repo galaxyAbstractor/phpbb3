04968154fa8b457d6c26a5e699d4b585c06e184c
Victor Nagy <galaxyAbstractor@gmail.com>
2013-07-12 13:13:43 -0400

    [feature/api] Renamed model to repository
    Renamed model to repository and updated classes using these.
    
    Added an entity for an API response which would contain HTTP
    status codes, data and other information.
    
    Updated path to retrieve topics list, it's now
    api/forums/{forum_id}/topics/{page}.
    
    PHPBB3-11595

--- phpBB/phpbb/controller/api/forum.php
+++ phpBB/phpbb/controller/api/forum.php
@@ -15,10 +15,6 @@ if (!defined('IN_PHPBB'))
 	exit;
 }
 
-$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : './';
-$phpEx = substr(strrchr(__FILE__, '.'), 1);
-
-
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
@@ -29,40 +25,48 @@ use Symfony\Component\Serializer\Encoder\JsonEncoder;
  */
 class phpbb_controller_api_forum
 {
-
 	/**
 	 * API Model
-	 * @var phpbb_model_api_forum
+	 * @var phpbb_model_repository_forum
 	 */
-	protected $model;
+	protected $forum_repository;
 
 	/**
 	 * Constructor
 	 *
-	 * @param phpbb_model_api_forum $model
+	 * @param phpbb_model_repository_forum $forum_repository
 	 */
-	function __construct(phpbb_model_api_forum $model)
+	function __construct(phpbb_model_repository_forum $forum_repository)
 	{
-		$this->model = $model;
+		$this->forum_repository = $forum_repository;
 	}
 
 	/**
 	 * Controller method to return a list of forums
 	 *
-	 * Accesible trough /api/forums/{forum_id} (no {forum_id} defaults to 0)
+	 * Accessible trough /api/forums/{forum_id} (no {forum_id} defaults to 0)
 	 * Method: GET
 	 *
-	 * @param $forum_id The forum to fetch, 0 fetches everything
+	 * @param int $forum_id The forum to fetch, 0 fetches everything
 	 * @return Response an array of forums, serialized to json
 	 */
 	public function forums($forum_id)
 	{
-		$forums = $this->model->get($forum_id);
+		$forums = $this->forum_repository->get($forum_id);
 
-		$serializer = new Serializer(array(new phpbb_model_normalizer_forum()), array(new JsonEncoder()));
-		$json = $serializer->serialize($forums, 'json');
+		$serializer = new Serializer(array(
+			new phpbb_model_normalizer_api_response(),
+			new phpbb_model_normalizer_forum(),
+		), array(new JsonEncoder()));
 
-		return new Response($json);
+		$response = new phpbb_model_entity_api_response(array(
+			'status' => 200,
+			'data' => $serializer->normalize($forums),
+		));
+
+		$json = $serializer->serialize($response, 'json');
+
+		return new Response($json, $response->get('status'));
 	}
 
 }
