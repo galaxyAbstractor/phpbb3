cde23fa0b3c8652d0e101ca0c1dc9ba6f0182147
Victor Nagy <galaxyAbstractor@gmail.com>
2013-12-18 20:19:47 +0100

    [feature/api] Added API flag to posts
    Added a way to know if a post was made using an API.
    TODO: Add icon to posts made by the API for moderators
    and administrators to see.
    
    PHPBB3-11595

--- phpBB/develop/create_schema_files.php
+++ phpBB/develop/create_schema_files.php
@@ -1418,6 +1418,7 @@ function get_schema_struct()
 			'post_delete_time'		=> array('TIMESTAMP', 0),
 			'post_delete_reason'	=> array('STEXT_UNI', ''),
 			'post_delete_user'		=> array('UINT', 0),
+			'post_api_post'		=> array('BOOL', 0),
 		),
 		'PRIMARY_KEY'	=> 'post_id',
 		'KEYS'			=> array(
